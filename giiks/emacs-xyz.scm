1406163193cf199970012a74587578cee3f39bfb
li <@li:maisiliym.uniks>
2021-05-23 11:01:44 +0700

    (snap shenMode)

--- giiks/emacs-xyz.scm
+++ giiks/emacs-xyz.scm
@@ -74,3 +74,42 @@ single theme but a set of guidelines with numerous implementations.")
     (description "")
     (license license:gpl3+)))
 
+(define-public emacs-shen-mode
+  (package
+    (name "emacs-shen-mode")
+    (version "head")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/NHALX/shen-mode")
+             (commit "43726db15c2a75dc6fe02eb215561f0a80f0f61c")))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32 "00i11z8pswwlcsdpnv0kwp3m1hk6grcz71ay01khx958n4x76fw7"))))
+    (build-system emacs-build-system)
+    (home-page "https://github.com/NHALX/shen-mode")
+    (synopsis "An Emacs major mode for editing Shen source code")
+    (description
+     "")
+    (license license:gpl3)))
+
+(define-public emacs-shen-elisp
+  (package
+    (name "emacs-shen-elisp")
+    (version "head")
+    (source
+     (origin
+       (method git-fetch)
+       (uri (git-reference
+             (url "https://github.com/deech/shen-elisp")
+             (commit "73b74c8d6e3a2ea34b667d177d9f130765bfe501")))
+       (file-name (git-file-name name version))
+       (sha256
+        (base32 "1ym048cmkghx373fb7n5m6r73q5nfa62m10mqr4nzhsizgyzdbrn"))))
+    (build-system emacs-build-system)
+    (home-page "https://github.com/deech/shen-elisp")
+    (synopsis "")
+    (description
+     "")
+    (license license:bsd-3)))
