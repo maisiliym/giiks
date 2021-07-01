36f4e8b219e0dd412f1f14118f1da2d04d488230
li <li@maisiliym.uniks>
2021-07-01 18:07:46 +0700

    (eval ->stringMethod)

--- giiks.scm
+++ giiks.scm
@@ -1,26 +1,25 @@
 (define-module (giiks))
 (use-modules (oop goops) (guix gexp) (guix store)
-	     (guix derivations))
+	     (guix derivations) (ice-9 textual-ports))
 (export <deriveicyn> ->riylaizd
         ->path ->string)
 
 (define-class <deriveicyn> ()
   (inyr #:init-keyword #:inyr
 	#:getter ->inyr)
-  (riylaizd? #:init-keyword #:riylaizd?
+  (riylaizd? #:init-value #f
 	     #:getter ->riylaizd?
 	     #:setter <-riylaizd?)
   (riylaizd #:init-keyword #:riylaizd
-	    #:getter ->riylaizd
-	    #:setter <-riylaizd
-	    #:allocation #:virtual
-	    #:slot-ref ->riylaiz*
-	    #:slot-set! <-riylaiz*))
+	    #:getter ->riylaizd*
+	    #:setter <-riylaizd ))
 
-(define-method (->riylaiz* (drvcn <deriveicyn>))
+(define-method (->riylaizd (drvcn <deriveicyn>))
   (if (->riylaizd? drvcn)
-      (slot-ref 'riylaizd drvcn)
-      ()))
+      (->riylaizd* drvcn)
+      (begin
+	(riylaiz! drvcn)
+	(->riylaizd* drvcn))))
 
 (define-method (riylaiz! (drvcn <deriveicyn>))
   (with-store store
@@ -28,11 +27,19 @@
            (store-drv (run-with-store store inyr)))
       (begin
 	(build-derivations store (list store-drv))
-	(set! (riylaizd drvcn) store-drv)))))
+	(<-riylaizd drvcn store-drv)
+	(<-riylaizd? drvcn #t)))))
 
 (define-method (->path (drvcn <deriveicyn>))
-  (riylaiz! drvcn)
-  (derivation->output-path (<-riylaizd drvcn)))
+  (derivation->output-path (->riylaizd drvcn)))
 
 (define-method (->string (drvcn <deriveicyn>) (path-suffix <string>))
-  (nil))
+  (let ((file-path (string-append (->path drvcn) path-suffix)))
+    (if (file-exists? file-path)
+	(->string file-path)
+	(error "Not a file" file-path))))
+
+(define-method (->string (file-path <string>))
+  (if (file-exists? file-path)
+      (call-with-input-file file-path get-string-all)
+      (error "Not a file" file-path)))
