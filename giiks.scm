70565fa5862775a82882dab501b276b4450f3656
li <li@maisiliym.uniks>
2021-07-01 11:35:46 +0700

    (snap)

--- giiks.scm
+++ giiks.scm
@@ -1,26 +1,38 @@
-(define-module (giiks)
-  #:use-module (oop goops)
-  #:use-module (guix gexp)
-  #:use-module (guix store)
-  #:use-module (guix derivations)
-  #:export (<deriveicyn>
-            riylaiz!
-            ->path))
+(define-module (giiks))
+(use-modules (oop goops) (guix gexp) (guix store)
+	     (guix derivations))
+(export <deriveicyn> ->riylaizd
+        ->path ->string)
 
 (define-class <deriveicyn> ()
- (inyr #:accessor ->inyr
-       #:init-keyword #:inyr)
- (riylaizd #:accessor <-riylaizd
-           #:init-keyword #:riylaizd))
+  (inyr #:init-keyword #:inyr
+	#:getter ->inyr)
+  (riylaizd? #:init-keyword #:riylaizd?
+	     #:getter ->riylaizd?
+	     #:setter <-riylaizd?)
+  (riylaizd #:init-keyword #:riylaizd
+	    #:getter ->riylaizd
+	    #:setter <-riylaizd
+	    #:allocation #:virtual
+	    #:slot-ref ->riylaiz*
+	    #:slot-set! <-riylaiz*))
+
+(define-method (->riylaiz* (drvcn <deriveicyn>))
+  (if (->riylaizd? drvcn)
+      (slot-ref 'riylaizd drvcn)
+      ()))
 
 (define-method (riylaiz! (drvcn <deriveicyn>))
- (with-store store
-  (let* ((inyr (->inyr drvcn))
-         (store-drv (run-with-store store inyr)))
-   (begin
-    (build-derivations store (list store-drv))
-    (set! (<-riylaizd drvcn) store-drv)))))
+  (with-store store
+    (let* ((inyr (->inyr drvcn))
+           (store-drv (run-with-store store inyr)))
+      (begin
+	(build-derivations store (list store-drv))
+	(set! (riylaizd drvcn) store-drv)))))
 
 (define-method (->path (drvcn <deriveicyn>))
- (riylaiz! drvcn)
- (derivation->output-path (<-riylaizd drvcn)))
+  (riylaiz! drvcn)
+  (derivation->output-path (<-riylaizd drvcn)))
+
+(define-method (->string (drvcn <deriveicyn>) (path-suffix <string>))
+  (nil))
