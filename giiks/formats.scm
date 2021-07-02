4bcb8178ffc623c11934b9d48e797ece16bc0818
li <li@maisiliym.uniks>
2021-07-02 12:12:57 +0700

    (eval betterMakeIniFile)

--- giiks/formats.scm
+++ giiks/formats.scm
@@ -1,6 +1,7 @@
 (define-module (giiks formats))
-(use-modules ((ice-9 match)))
-(export (newline-strings make-ini))
+(use-modules (ice-9 match)
+	      (guix gexp))
+(export newline-strings make-ini make-ini-file)
 
 (define (newline-strings strings)
   (string-join strings "\n"))
@@ -19,11 +20,17 @@
 (define (make-ini-section section)
   (match section
     ((name content)
-     (string-append
-      "[" name "]\n"
-      (newline-strings
-       (map make-ini-value content))))))
-
+     (match content
+       ((value ...)
+	(string-append
+	 "[" name "]\n"
+	 (newline-strings
+	  (map make-ini-value content))))
+       (string
+	(make-ini-value section))))))
 
 (define (make-ini config)
   (newline-strings (map make-ini-section config)))
+
+(define (make-ini-file name config)
+  (plain-file name (make-ini config)))
