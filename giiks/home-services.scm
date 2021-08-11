6e54b97b48feb57cc969790dbec18db6f965d7d3
li <li@maisiliym.uniks>
2021-08-11 08:48:31 +0700

    (snap [add homeServices dbus pipewire])

---
+++ giiks/home-services.scm
@@ -0,0 +1,104 @@
+(define-module (giiks home-services)
+  #:use-module (gnu home-services)
+  #:use-module (gnu home-services shepherd)
+  #:use-module (guix gexp)
+  #:use-module (gnu packages linux)
+  #:use-module (gnu home-services files))
+
+(define (home-dbus-environment-variables-service _)
+  '(("DBUS_SESSION_BUS_ADDRESS" . "unix:path=$XDG_RUNTIME_DIR/dbus.sock")))
+
+(define (home-dbus-shepherd-service _)
+  (list
+   (shepherd-service
+    (provision '(dbus-home))
+    (stop  #~(make-kill-destructor))
+    (start #~(make-forkexec-constructor
+              (list #$(file-append (@@ (gnu packages glib) dbus)
+                                   "/bin/dbus-daemon")
+                    "--nofork"
+                    "--session"
+                    (string-append
+                     "--address=" "unix:path="
+                     (getenv "XDG_RUNTIME_DIR") "/dbus.sock")))))))
+
+(define-public home-dbus-service-type
+  (service-type
+   (name 'home-dbus)
+   (extensions
+    (list (service-extension
+           home-environment-variables-service-type
+           home-dbus-environment-variables-service)
+          (service-extension
+           home-shepherd-service-type
+           home-dbus-shepherd-service)))
+   (default-value #f)
+   (description "run dbus")))
+
+(define (home-pipewire-files-service _)
+  `(("config/alsa/asoundrc"
+     ,(mixed-text-file
+       "asoundrc"
+       #~(string-append
+          "<"
+          #$(file-append
+             pipewire-0.3
+             "/share/alsa/alsa.conf.d/50-pipewire.conf")
+          ">\n<"
+          #$(file-append
+             pipewire-0.3
+             "/share/alsa/alsa.conf.d/99-pipewire-default.conf")
+          ">\n"
+          "
+pcm_type.pipewire {
+  lib " #$(file-append
+           pipewire-0.3
+           "/lib/alsa-lib/libasound_module_pcm_pipewire.so") "
+}
+
+ctl_type.pipewire {
+  lib " #$(file-append
+           pipewire-0.3
+           "/lib/alsa-lib/libasound_module_ctl_pipewire.so") "
+}
+")))))
+
+(define (home-pipewire-shepherd-service _)
+  (list
+   (shepherd-service
+    (requirement '(dbus-home))
+    (provision '(pipewire))
+    (stop  #~(make-kill-destructor))
+    (start #~(make-forkexec-constructor
+              (list #$(file-append pipewire-0.3 "/bin/pipewire")))))
+   (shepherd-service
+    (requirement '(pipewire))
+    (provision '(pipewire-media-session))
+    (stop  #~(make-kill-destructor))
+    (start #~(make-forkexec-constructor
+              (list #$(file-append pipewire-0.3 "/bin/pipewire-media-session")))))
+   (shepherd-service
+    (requirement '(pipewire))
+    (provision '(pipewire-pulse))
+    (stop  #~(make-kill-destructor))
+    (start #~(make-forkexec-constructor
+              (list #$(file-append pipewire-0.3 "/bin/pipewire-pulse")))))))
+
+(define (home-pipewire-environment-variables-service _)
+  '(("RTC_USE_PIPEWIRE" . "true")))
+
+(define-public home-pipewire-service-type
+  (service-type
+   (name 'home-pipewire)
+   (extensions
+    (list (service-extension
+           home-files-service-type
+           home-pipewire-files-service)
+          (service-extension
+           home-shepherd-service-type
+           home-pipewire-shepherd-service)
+          (service-extension
+           home-environment-variables-service-type
+           home-pipewire-environment-variables-service)))
+   (default-value #f)
+   (description "run pipewire and stuff")))
