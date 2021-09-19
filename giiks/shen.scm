25b0b0cba9283ea49d5cafeb967df61ddcfb7a65
li <li@maisiliym.uniks>
2021-09-19 08:35:24 +0700

    (snap (wip shenChez))

--- giiks/shen.scm
+++ giiks/shen.scm
@@ -3,6 +3,8 @@
  (guix packages) (guix gexp) (guix download)
  (guix git-download) (guix build-system gnu)
  (gnu packages) (gnu packages lisp)
+ ((gnu packages chez) #:select (chez-scheme))
+ ((gnu packages linux) #:select (util-linux))
  (srfi srfi-1) (ice-9 match))
 
 (define-public shen-sources
@@ -53,3 +55,85 @@
     (synopsis "TBC")
     (description "TBC")
     (license #f)))
+
+(define-public shen-chez-bootstrap
+  (let ((version "0.24"))
+    (package
+      (name "shen-chez-bootstrap")
+      (version version)
+      (source
+       (origin
+	 (method url-fetch)
+	 (uri (string-append
+               "https://github.com/tizoc/shen-scheme/releases/download/v"
+	       version "/shen-scheme-v" version "-linux-bin.tar.gz"))
+	 (sha256
+          (base32
+           "0s3mrkmqz8f0df3djl5k0dvkhw00i6fc2r21mg4661s0rxgxa6dg"))))
+      (build-system gnu-build-system)
+      (arguments
+       `(#:tests? #f
+	 #:phases
+	 (modify-phases %standard-phases
+	   (add-after 'unpack 'link-shen-sources
+	     (let* ((kernel (assoc-ref %build-inputs "shen-sources")))
+	       (lambda _ (symlink kernel "kl"))))
+	   (replace 'configure (lambda _ #t))
+	   (replace 'build
+	     (let*
+		 ((out (assoc-ref %outputs "out"))
+		  (bin (string-append out "/bin")))
+	       (lambda _
+		 (mkdir-p bin))))
+	   (replace 'install
+	     (let* ((out (assoc-ref %outputs "out"))
+		    (install-path (string-append out "/bin")))
+	       (lambda _
+		 (install-file "bin/shen-scheme" install-path)))))))
+      (inputs
+       `(("libuuid" ,util-linux "lib")
+	 ("shen-sources" ,shen-sources)
+	 ("chez-scheme" ,chez-scheme)))
+      (home-page "https://github.com/Shen-Language/shen-cl")
+      (synopsis "TBC")
+      (description "TBC")
+      (license #f))))
+
+(define-public shen-chez
+  (let ((version "0.24")
+	(commit "908ec69c8e45d90eef6dca6c709cdaa30dcb760f")
+        (revision "1"))
+    (package
+      (name "shen-chez")
+      (version (git-version version revision commit))
+      (source
+       (origin
+	 (method git-fetch)
+	 (uri (git-reference
+               (url "https://github.com/tizoc/shen-scheme")
+               (commit commit)))
+	 (sha256
+          (base32
+           "1041s8sb1yscyp8b5y79cxv2maja27lcdbq9g0116y2ji2qdmm5l"))))
+      (build-system gnu-build-system)
+      (arguments
+       `(;#:tests? #f ;tests are run in build phase
+	 #:phases
+	 (modify-phases %standard-phases
+	   (add-after 'unpack 'link-shen-sources
+	     (let* ((kernel (assoc-ref %build-inputs "shen-sources")))
+	       (lambda _ (symlink kernel "kl"))))
+	   (replace 'configure (lambda _ #t))
+	   (replace 'build (lambda _ (invoke "make" "chez")))
+	   (replace 'install
+	     (let* ((out (assoc-ref %outputs "out"))
+		    (install-path (string-append out "/bin")))
+	       (lambda _
+		 (install-file "bin/chez/shen" install-path)))))))
+      (inputs
+       `(("shen-sources" ,shen-sources)
+	 ("chez-scheme" ,chez-scheme)))
+      (home-page "https://github.com/tizoc/shen-scheme")
+      (synopsis "TBC")
+      (description "TBC")
+      (license #f))))
