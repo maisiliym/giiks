a6e4f2a0b7193c422fab4fc20c145ce0d88fc60d
li <li@maisiliym.uniks>
2021-09-20 15:30:25 +0700

    (snap [(built shenKlambda using shenChezBootstrap) refactor])

--- giiks/shen.scm
+++ giiks/shen.scm
@@ -9,22 +9,9 @@
  ((gnu packages linux) #:select (util-linux))
  (srfi srfi-1) (ice-9 match))
 
-(define-public shen-sources
-  (let ((commit "e18789684218ca036a4e3e7360b952b642d6a144")
-	(version "22.4"))
-    (origin
-      (method git-fetch)
-      (uri (git-reference
-            (url "https://github.com/Shen-Language/shen-sources")
-            (commit commit)))
-      (file-name (git-file-name "shen-sources" version))
-      (sha256
-       (base32
-        "1dzizvf14c6mkxyvpbhs2qkyg0lfm7liaqhs9sg6s734syiqrq7i")))))
-
-(define-public shen-sbcl
+(define-public shen-sbcl-bootstrap
   (package
-    (name "shen-sbcl")
+    (name "shen-sbcl-bootstrap")
     (version "3.0.3")
     (source
      (origin
@@ -77,21 +64,17 @@
        `(#:tests? #f
 	 #:phases
 	 (modify-phases %standard-phases
-	   (add-after 'unpack 'link-shen-sources
-	     (let* ((kernel (assoc-ref %build-inputs "shen-sources")))
-	       (lambda _ (symlink kernel "kl"))))
 	   (replace 'configure (lambda _ #t))
 	   (replace 'build
 	     (let*
 		 ((out (assoc-ref %outputs "out"))
 		  (bin (string-append out "/bin"))
-		  (util-linux-lib (assoc-ref %build-inputs "util-linux-lib"))
+		  (sbcl-lib (assoc-ref %build-inputs "sbcl-lib"))
 		  (libc (assoc-ref %build-inputs "libc"))
 		  (ld-so (string-append libc ,(glibc-dynamic-linker)))
-		  (rpath (string-append util-linux-lib "/lib" ":"
+		  (rpath (string-append sbcl-lib "/lib" ":"
 					libc "/lib")))
 	       (lambda _
-		 
 		 (invoke "patchelf" "--set-interpreter" ld-so
 			 "--set-rpath" rpath "bin/shen-scheme"))))
 	   (replace 'install
@@ -99,19 +82,62 @@
 		    (bin-path (string-append out "/bin"))
 		    (boot-file-path (string-append out "/lib/shen-scheme/")))
 	       (lambda _
-		 (mkdir-p bin-path)
-		 (mkdir-p boot-file-path)
-		 (install-file "bin/shen-scheme" bin-path)
+		 (rename-file "bin/shen-scheme" "bin/shen")
+		 (install-file "bin/shen" bin-path)
 		 (install-file "lib/shen-scheme/shen.boot"
 			       boot-file-path)))))))
       (inputs
        `(("patchelf" ,patchelf)
-	 ("util-linux-lib" ,util-linux "lib")
-	 ("shen-sources" ,shen-sources)))
-      (home-page "https://github.com/Shen-Language/shen-cl")
+	 ("sbcl-lib" ,util-linux "lib")))
+      (home-page "https://github.com/tizoc/shen-scheme")
       (synopsis "TBC")
       (description "TBC")
       (license #f))))
+
+(define-public shen shen-chez-bootstrap)
+
+(define shen-sources-commit "e18789684218ca036a4e3e7360b952b642d6a144")
+(define shen-sources-version "22.4")
+
+(define shen-sources
+  (origin
+    (method git-fetch)
+    (uri (git-reference
+          (url "https://github.com/Shen-Language/shen-sources")
+          (commit shen-sources-commit)))
+    (file-name (git-file-name "shen-sources" shen-sources-version))
+    (sha256
+     (base32
+      "1dzizvf14c6mkxyvpbhs2qkyg0lfm7liaqhs9sg6s734syiqrq7i"))))
+
+(define-public shen-klambda
+  (package
+    (name "shen-klambda")
+    (version shen-sources-version)
+    (source shen-sources)
+    (build-system gnu-build-system)
+    (arguments
+     `(#:phases
+       (modify-phases %standard-phases
+	 (replace 'configure (lambda _ #t))
+	 (replace 'build
+	   (let* ((out (assoc-ref %outputs "out")))
+	     (lambda _
+	       (mkdir-p "klambda")
+	       (invoke "shen" "eval" "-l"
+		       "make.shen" "-e" "(make)"))))
+	 (replace 'install
+	   (let* ((out (assoc-ref %outputs "out"))
+		  (install-path (string-append out "/lib/klambda")))
+	     (lambda _
+	       (copy-recursively "klambda" install-path)))))
+       #:tests? #f))
+    (inputs
+     `(("shen" ,shen)))
+    (home-page "https://github.com/Shen-Language/shen-sources")
+    (synopsis "TBC")
+    (description "TBC")
+    (license #f)))
 
 (define-public shen-chez
   (let ((version "0.24")
@@ -131,21 +157,35 @@
            "1041s8sb1yscyp8b5y79cxv2maja27lcdbq9g0116y2ji2qdmm5l"))))
       (build-system gnu-build-system)
       (arguments
-       `(;#:tests? #f ;tests are run in build phase
-	 #:phases
+       `(#:phases
 	 (modify-phases %standard-phases
-	   (add-after 'unpack 'link-shen-sources
-	     (let* ((kernel (assoc-ref %build-inputs "shen-sources")))
-	       (lambda _ (symlink kernel "kl"))))
-	   (replace 'configure (lambda _ #t))
-	   (replace 'build (lambda _ (invoke "make" "chez")))
+	   (replace 'configure
+	     (let* ((klambda (string-append
+			      (assoc-ref %build-inputs "shen-klambda")
+			      "/lib/klambda")))
+	       (lambda _
+		 (delete-file-recursively "kl")
+		 (copy-recursively klambda "kl"))))
+	   (replace 'build
+	     (let* ((out (assoc-ref %outputs "out")))
+	       (lambda _
+		 (invoke "shen" "script"
+			 "scripts/do-build.shen"))))
+	   (replace 'check
+	     (let* ((out (assoc-ref %outputs "out"))
+		    (install-path (string-append out "/bin")))
+	       (lambda _
+		 (invoke "./shen-scheme" "script"
+			 "scripts/run-shen-tests.shen"))))
 	   (replace 'install
 	     (let* ((out (assoc-ref %outputs "out"))
 		    (install-path (string-append out "/bin")))
 	       (lambda _
-		 (install-file "bin/chez/shen" install-path)))))))
+		 (install-file "shen-scheme" install-path))))
+	   )))
       (inputs
-       `(("shen-sources" ,shen-sources)
+       `(("shen-klambda" ,shen-klambda)
+	 ("shen-chez-bootstrap" ,shen-chez-bootstrap)
 	 ("chez-scheme" ,chez-scheme)))
       (home-page "https://github.com/tizoc/shen-scheme")
       (synopsis "TBC")
